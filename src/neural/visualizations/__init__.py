8bfc0120168db8189dc9132bffb6cb92fca79292
Tristan Tibbs <tristantibbs@gmail.com>
2024-12-27 18:54:48 +0000

    Extracted visualizatin code into its own subpackage

---
+++ src/neural/visualizations/__init__.py
@@ -0,0 +1,8 @@
+"""
+The `visualizations` subpackage provides real-time training 
+visualization support for the Neural Noodle project.
+"""
+
+from .plotting import TrainingPlotter
+
+__all__ = ["TrainingPlotter"]
